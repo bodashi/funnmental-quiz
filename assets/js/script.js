acb9f718503a7fa21af01edeb88ba969bbf4e92f
Nicolas Young <n.colasyoung@gmail.com>
2022-10-31 19:13:32 -0500

    create quiz questions array with question object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,36 @@
 var startQuiz = document.querySelector("#start-quiz");
 var startTimer = document.querySelector("#quizTimer");
+var welcomeEl = document.querySelector("#welcome");
 
-var timerCount = 60;
+var timerCount = 5;
+
+var quizQuestions = [
+  {
+    question: "Which of these does not declare a variable?",
+    answers: ["let", "const", "var", "//"],
+    correctAnswer: "//"
+  }
+]
 
 var beginQuiz = function () {
-  
+  welcomeEl.remove();
+
   var timerCountdown = function () {
-    quizTimer.innerHTML = timerCount;
+    startTimer.textContent = timerCount;
     
     if (timerCount === 0) {
-      alert("Time's Up!!");
+      
       clearInterval(startCountdown);
+      startTimer.textContent = "Time's Up!!";
     }
     timerCount--;
   };
-
+  
   var startCountdown = setInterval(timerCountdown, 1000);
+
+  for (var i = 0; i < quizQuestions.length; i++) {
+    console.log(quizQuestions[i]);
+  }
 };
 
 
